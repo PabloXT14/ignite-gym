a561a58df59a00106449467b777eef9809449358
Pablo Alan <pabloxt14@gmail.com>
2024-12-04 18:24:51 -0300

    feat: customize status-bar

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
+import { StyleSheet, Text, View, StatusBar } from 'react-native'
+
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
-import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -13,11 +14,12 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      {fontsLoaded ? (
-        <Text style={{ fontFamily: 'Roboto_700Bold', fontSize: 20 }}>Home</Text>
-      ) : (
-        <View />
-      )}
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Text style={styles.text}>Home</Text> : <View />}
     </View>
   )
 }
@@ -25,8 +27,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#202024',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#fafafa',
+    fontSize: 20,
+    fontFamily: 'Roboto_400Regular',
   },
 })
