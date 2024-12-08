2c5d4d14cfd393df31d4f59d9fad46bdd21a466e
Pablo Alan <pabloxt14@gmail.com>
2024-12-08 18:12:56 -0300

    feat: styling focus on input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -7,7 +7,10 @@ type InputProps = ComponentProps<typeof InputField>
 
 export function Input({ ...props }: InputProps) {
   return (
-    <GluestackInput className="bg-gray-700 h-14 px-4 border-0 rounded-md">
+    <GluestackInput
+      className="bg-gray-700 h-14 px-4 border border-transparent rounded-md data-[focus=true]:border-green-500"
+      focusable
+    >
       <InputField
         className="text-white font-regular"
         placeholderTextColor={colors.gray[300]}
