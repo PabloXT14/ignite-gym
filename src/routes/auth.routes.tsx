c5510846db3e0f152f1c35630fa2efe631b46759
Pablo Alan <pabloxt14@gmail.com>
2024-12-17 09:54:06 -0300

    feat: create public routes

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,16 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import { SignIn } from '../screens/sign-in'
+import { SignUp } from '../screens/sign-up'
+
+const { Navigator, Screen } = createNativeStackNavigator()
+
+export function AuthRoutes() {
+  return (
+    <Navigator>
+      <Screen name="sign-in" component={SignIn} />
+
+      <Screen name="sign-up" component={SignUp} />
+    </Navigator>
+  )
+}
