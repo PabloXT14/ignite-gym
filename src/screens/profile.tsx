84419056d90b6079d144fcdd535b64b39abb8d55
Pablo Alan <pabloxt14@gmail.com>
2024-12-18 19:03:43 -0300

    feat: add user photo to profile

--- src/screens/profile.tsx
+++ src/screens/profile.tsx
@@ -1,10 +1,25 @@
+import { ScrollView } from 'react-native'
+
 import { VStack } from '@/components/ui/vstack'
+import { Center } from '@/components/ui/center'
+
 import { ScreenHeader } from '../components/screen-header'
+import { UserPhoto } from '../components/user-photo'
 
 export function Profile() {
   return (
     <VStack className="flex-1">
       <ScreenHeader title="Perfil" />
+
+      <ScrollView contentContainerClassName="pb-9">
+        <Center className="mt-6 px-10">
+          <UserPhoto
+            source={'https://github.com/pabloxt14.png'}
+            alt="Imagem do Usuário"
+            size="xl"
+          />
+        </Center>
+      </ScrollView>
     </VStack>
   )
 }
