78404ea316f0671e48e22c9446fe61872796e25d
Pablo Alan <pabloxt14@gmail.com>
2024-12-18 10:39:34 -0300

    feat: show selected group

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,14 +1,31 @@
+import { useState } from 'react'
+
 import { VStack } from '@/components/ui/vstack'
+import { HStack } from '@/components/ui/hstack'
 
 import { HomeHeader } from '../components/home-header'
 import { Group } from '../components/group'
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costas')
+
   return (
     <VStack className="flex-1">
       <HomeHeader />
 
-      <Group name="Costas" isActive={false} />
+      <HStack className="gap-3 mt-10 ml-8">
+        <Group
+          name="Costas"
+          isActive={groupSelected === 'costas'}
+          onPress={() => setGroupSelected('costas')}
+        />
+
+        <Group
+          name="Ombro"
+          isActive={groupSelected === 'ombro'}
+          onPress={() => setGroupSelected('ombro')}
+        />
+      </HStack>
     </VStack>
   )
 }
