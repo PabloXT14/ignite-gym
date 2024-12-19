b320528bd8a26483bdccdd3fa1f4656df39c47c3
Pablo Alan <pabloxt14@gmail.com>
2024-12-19 19:11:23 -0300

    feat: (exercise screen) create go back button

--- src/screens/exercise.tsx
+++ src/screens/exercise.tsx
@@ -1,10 +1,26 @@
-import { Center } from '@/components/ui/center'
-import { Text } from '@/components/ui/text'
+import { TouchableOpacity } from 'react-native'
+import { ArrowLeft } from 'lucide-react-native'
+import { useNavigation } from '@react-navigation/native'
+
+import { VStack } from '@/components/ui/vstack'
+import { Icon } from '@/components/ui/icon'
+
+import type { AppNavigatorRoutesProps } from '../routes/app.routes'
 
 export function Exercise() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  function handleGoBack() {
+    navigation.goBack()
+  }
+
   return (
-    <Center className="flex-1">
-      <Text className="text-white">Exercise</Text>
-    </Center>
+    <VStack className="flex-1">
+      <VStack className="px-8 bg-gray-600 pt-12">
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon as={ArrowLeft} className="text-green-500" size="xl" />
+        </TouchableOpacity>
+      </VStack>
+    </VStack>
   )
 }
