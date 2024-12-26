2af0c200314f4bbdb232993ceefdfd78f5a4c2ac
Pablo Alan <pabloxt14@gmail.com>
2024-12-26 16:08:26 -0300

    feat: get response from back-end

--- mobile/src/screens/sign-up.tsx
+++ mobile/src/screens/sign-up.tsx
@@ -68,6 +68,8 @@ export function SignUp() {
       },
       body: JSON.stringify({ name, email, password }),
     })
+      .then(response => response.json())
+      .then(data => console.log(data))
   }
 
   return (
