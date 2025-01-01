448e92741b9a6ea2efe644e709414cbb3d492c06
Pablo Alan <pabloxt14@gmail.com>
2025-01-01 11:16:08 -0300

    feat: load user data on profile screen

--- mobile/src/screens/profile.tsx
+++ mobile/src/screens/profile.tsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 import { Alert, ScrollView, TouchableOpacity } from 'react-native'
 import * as ImagePicker from 'expo-image-picker'
 import * as FileSystem from 'expo-file-system'
+import { Controller, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 import { VStack } from '@/components/ui/vstack'
 import { Center } from '@/components/ui/center'
@@ -15,10 +18,39 @@ import { UserPhoto } from '../components/user-photo'
 import { Input } from '../components/input'
 import { Button } from '../components/button'
 import { ToastMessage } from '../components/toast-message'
+import { useAuth } from '../hooks/use-auth'
+
+const profileFormSchema = z.object({
+  name: z
+    .string({ required_error: 'O nome é obrigatório' })
+    .min(3, { message: 'O nome precisa ter pelo menos 3 letras' }),
+  email: z
+    .string({ required_error: 'O e-mail é obrigatório' })
+    .email({ message: 'Formato de e-mail inválido' }),
+  old_password: z.string(),
+  new_password: z
+    .string()
+    .min(6, { message: 'A nova senha precisa ter pelo menos 6 letras' }),
+  confirm_new_password: z
+    .string()
+    .min(6, { message: 'A nova senha precisa ter pelo menos 6 letras' }),
+})
+
+type ProfileFormData = z.infer<typeof profileFormSchema>
 
 const MAX_IMAGE_SIZE_MB = 5
 
 export function Profile() {
+  const { user } = useAuth()
+
+  const { control, handleSubmit } = useForm<ProfileFormData>({
+    resolver: zodResolver(profileFormSchema),
+    defaultValues: {
+      name: user.name,
+      email: user.email,
+    },
+  })
+
   const [userPhoto, setUserPhoto] = useState('https://github.com/pabloxt14.png')
 
   const toast = useToast()
@@ -70,6 +102,26 @@ export function Profile() {
     }
   }
 
+  async function handleProfileUpdate(data: ProfileFormData) {
+    try {
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+
+      toast.show({
+        placement: 'top',
+        render: ({ id }) => (
+          <ToastMessage
+            id={id}
+            action="error"
+            title="Erro ao atualizar perfil"
+            onClose={() => toast.close(id)}
+          />
+        ),
+      })
+    }
+  }
+
   return (
     <VStack className="flex-1">
       <ScreenHeader title="Perfil" />
@@ -89,11 +141,30 @@ export function Profile() {
           </TouchableOpacity>
 
           <Center className="w-full gap-4">
-            <Input placeholder="Nome" className="bg-gray-600" />
-            <Input
-              value="johndoe@email.com"
-              className="bg-gray-600"
-              isReadOnly
+            <Controller
+              control={control}
+              name="name"
+              render={({ field: { value, onChange } }) => (
+                <Input
+                  placeholder="Nome"
+                  className="bg-gray-600"
+                  value={value}
+                  onChangeText={onChange}
+                />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="email"
+              render={({ field: { value, onChange } }) => (
+                <Input
+                  value={value}
+                  onChangeText={onChange}
+                  className="bg-gray-600"
+                  isReadOnly
+                />
+              )}
             />
           </Center>
 
@@ -102,25 +173,53 @@ export function Profile() {
           </Heading>
 
           <Center className="w-full gap-4">
-            <Input
-              placeholder="Senha antiga"
-              className="bg-gray-600"
-              secureTextEntry
+            <Controller
+              control={control}
+              name="old_password"
+              render={({ field: { value, onChange } }) => (
+                <Input
+                  placeholder="Senha antiga"
+                  className="bg-gray-600"
+                  secureTextEntry
+                  value={value}
+                  onChangeText={onChange}
+                />
+              )}
             />
 
-            <Input
-              placeholder="Nova senha"
-              className="bg-gray-600"
-              secureTextEntry
+            <Controller
+              control={control}
+              name="new_password"
+              render={({ field: { value, onChange } }) => (
+                <Input
+                  placeholder="Nova senha"
+                  className="bg-gray-600"
+                  secureTextEntry
+                  value={value}
+                  onChangeText={onChange}
+                />
+              )}
             />
 
-            <Input
-              placeholder="Confirmar a nova senha"
-              className="bg-gray-600"
-              secureTextEntry
+            <Controller
+              control={control}
+              name="confirm_new_password"
+              render={({ field: { value, onChange } }) => (
+                <Input
+                  placeholder="Confirmar a nova senha"
+                  className="bg-gray-600"
+                  secureTextEntry
+                  value={value}
+                  onChangeText={onChange}
+                />
+              )}
             />
 
-            <Button title="Atualizar" className="mt-8" />
+            <Button
+              title="Atualizar"
+              className="mt-8"
+              onPress={handleSubmit(handleProfileUpdate)}
+            />
           </Center>
         </Center>
       </ScrollView>
